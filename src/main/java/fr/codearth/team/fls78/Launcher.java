f59ec355992967f6771373094583e9623b6a8b30
CodEarth <bbyanis2008@hotmail.fr>
2020-03-01 12:04:40 +0100

    Correction de bugs

--- src/main/java/fr/codearth/team/fls78/Launcher.java
+++ src/main/java/fr/codearth/team/fls78/Launcher.java
@@ -4,7 +4,7 @@ import fr.codearth.team.fls78.core.Events;
 
 public class Launcher {
     public static void main(String[] args) {
-        Events.EVENTS.setDev(true);
-        System.out.println(Events.EVENTS.getDev());
+        Events.setDev(true);
+        System.out.println(Events.getDev());
     }
 }
--- src/main/java/fr/codearth/team/fls78/core/Events.java
+++ src/main/java/fr/codearth/team/fls78/core/Events.java
@@ -3,37 +3,37 @@ package fr.codearth.team.fls78.core;
 
 public class Events {
     public static Events EVENTS;
-    public Boolean isDev;
-    public String platform;
-    public String args;
+    public static Boolean isDev;
+    public static String platform;
+    public static String args;
     public Events(Boolean isDev, String platform, String args) {
         EVENTS = this;
-        this.isDev = isDev;
-        this.platform = platform;
-        this.args = args;
+        Events.isDev = isDev;
+        Events.platform = platform;
+        Events.args = args;
     }
 
-    public void setDev(Boolean dev) {
-        isDev = dev;
+    public static void setDev(Boolean dev) {
+        Events.isDev = dev;
     }
 
     public void setPlatform(String platform) {
-        this.platform = platform;
+        Events.platform = platform;
     }
 
     public void setArgs(String args) {
-        this.args = args;
+        Events.args = args;
     }
 
-    public Boolean getDev() {
+    public static Boolean getDev() {
         return isDev;
     }
 
-    public String getPlatform() {
+    public static String getPlatform() {
         return platform;
     }
 
-    public String getArgs() {
+    public static String getArgs() {
         return args;
     }
 }
